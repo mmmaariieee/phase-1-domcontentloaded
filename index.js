39f43efe7713aaed2d5e4cba92b3b7c5d9d9cf08
Mariia <m.goncharenkoo@icloud.com>
2022-09-26 10:55:59 -0400

    commit tree d88297cd1baf36220d8d51f61c677bc26c62d54b

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const txt = document.getElementById("text");
+    txt.textContent = "This is really cool!";
+  });
